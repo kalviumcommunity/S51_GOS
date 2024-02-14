0e2d62d1b3f6f26145764baa7e307729289ee6cf
M-Ranjan <ranjan.m1325@gmail.com>
2024-02-14 11:01:08 +0530

    Added the basic express file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = "3000";
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+app.get('/', (req, res) => {
+    res.send('Home');
+  });
+  
+  app.get('/ping', (req, res) => {
+    res.send('pong');
+  });
+  
+
+module.exports = app;
