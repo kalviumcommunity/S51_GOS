bb7e9093df6b4e23d2651616cd7431f5bdd63fd6
M-Ranjan <ranjan.m1325@gmail.com>
2024-03-02 16:52:32 +0530

    Joi validation

---
+++ validator.js
@@ -0,0 +1,18 @@
+const Joi = require("joi");
+const validator = (schema) => (data) =>
+  schema.validate(data, { abortEarly: false });
+
+const postUpdateSchema = Joi.object({
+  SneakerID: Joi.number().required(),
+  Brand: Joi.string().required(),
+  Model: Joi.string().required(),
+  Type: Joi.string().required(),
+  Color: Joi.string().required(),
+  Size: Joi.number().required(),
+  Price: Joi.number().required(),
+  SneakerURL: Joi.string().required(),
+  Availability: Joi.string().required(),
+});
+
+const updateAndPostValidator = validator(postUpdateSchema);
+module.exports = updateAndPostValidator;
